952aaa900832b73f8119f43b5403dbaa010a91f2
Zack Scholl <zack.scholl@gmail.com>
2015-12-14 17:54:43 -0500

    Added setup.py

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sync-music-player
+
+Allows you to play music from your library, in sync, with various other computers.
+
+## Requirements
+
+- A good static file server for the music file (nginx / apache)
+- Python
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='syncmusic',
+      version='1.0',
+      description='Syncs music from web browser',
+      author='Zack',
+      author_email='zack@hypercubeplatforms.com',
+      url='https://github.com/schollz/sync-music-player',
+      install_requires=['eyed3', 'tornado', 'flask'],
+     )
