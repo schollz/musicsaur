3dc4853c3efbc50768a92c8f98d134cf53994f67
Zack Scholl <zack.scholl@gmail.com>
2015-12-14 09:29:21 -0500

    Works bettery

--- test.py
+++ test.py
@@ -1,71 +1,71 @@
 import time
 import sys
-import numpy
+import shutil
+import os
 
-import requests
-from flask import Flask, request, jsonify
+from flask import *
 
 app = Flask(__name__)
 app.debug = True
 
 
-next_song = time.time() + 15
+playlist = ['short1.mp3','short2.mp3','long2.mp3','long3.mp3','short3.mp3']
+current_song = -1
+last_activated = 0
+is_playing = False
+
+def getTime():
+    return int(time.time()*1000)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index_html():
+    return render_template('index.html')
 
 @app.route("/sync", methods=['GET', 'POST'])
 def sync():
     #searchword = request.args.get('key', '')
     if request.method == 'POST':
-        print(time.time())
+        print(getTime())
         data = {}
         data['client_timestamp'] = request.form['client_timestamp']
-        data['server_timestamp'] = time.time()
-        data['next_song'] = next_song
+        data['server_timestamp'] = getTime()
+        data['next_song'] = next_song_time
+        data['is_playing'] = is_playing
         return jsonify(data)
 
-def client():
-    import pygame
-    pygame.init()
-    pygame.mixer.music.load("test.wav")
-    correct_time_delta = []
-    next_trigger = 0
-    for i in range(5):
-        print(time.time())
-        r = requests.post('http://192.168.1.11:5000/sync',data = {'client_timestamp':time.time()})
-        data = r.json()
-        latency = time.time() - float(data['client_timestamp'])
-        half_latency = latency / 2.0
-        time_delta = time.time() - float(data['server_timestamp'])
-        next_trigger = float(data['next_song'])
-        correct_time_delta.append(time_delta + half_latency)
-        time.sleep(1)
-    print(correct_time_delta)
-    start_time= time.time() + 10
-    print('next trigger: ' + str(next_trigger))
-    print('cur time' + str(time.time() - numpy.mean(correct_time_delta)))
-    while True:
-        if time.time() - numpy.mean(correct_time_delta) > next_trigger:
-            print('done!')
-            print(time.time() - numpy.mean(correct_time_delta))
-            break
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
-    print('done playing music ' + str(time.time() - numpy.mean(correct_time_delta)))
-    time.sleep(20)
 
+@app.route("/nextsong", methods=['GET', 'POST'])
+def finished():
+    response = {'message':'loading!'}
+    if request.method == 'POST':
+        nextSong(6)
+    return jsonify(response)
 
+@app.route("/playing", methods=['GET', 'POST'])
+def playing():
+    global is_playing
+    response = {'message':'loading!'}
+    if request.method == 'POST':
+        is_playing = True
+    return jsonify(response)
+
+def nextSong(delay):
+    global last_activated
+    global current_song
+    global next_song_time
+    global is_playing
+    if time.time() - last_activated > 10:
+        is_playing = False
+        current_song += 1
+        last_activated = time.time()
+        shutil.copy('./' + playlist[current_song],'./static/')
+        os.rename('./static/' + playlist[current_song],'./static/sound.mp3')
+        os.system('scp ' + playlist[current_song] + ' phi@192.168.1.11:/www/data/sound.mp3')
+        next_song_time = getTime() + delay*1000
+        print ('next up: ' + playlist[current_song])
 
 if __name__ == "__main__":
-    if len(sys.argv)<2:
-        print "python timesync.py client/server IP"
-        sys.exit(1)
-    if sys.argv[1]=='server':
-        print('next song:' + str(next_song))
-        app.run(host='192.168.1.11')
-    else:
-        client()
+    nextSong(20)
+    app.run(host='192.168.1.2')
+
