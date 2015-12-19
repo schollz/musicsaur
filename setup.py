4d0e7e92702892c4088cbc41fa8ece4414d141a8
Zack Scholl <zack.scholl@gmail.com>
2015-12-19 06:25:09 -0500

    Version 1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='syncmusic',
-      version='1.1',
+      version='1.2',
       description='Syncs music from web browser',
       author='Zack',
       author_email='zack@hypercubeplatforms.com',
