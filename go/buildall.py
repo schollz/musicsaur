6b17d0ea3f9bcbcbe8a2986201b2c881bf10770e
Zack Scholl <zack.scholl@gmail.com>
2015-12-22 22:57:22 -0500

    Adding building

---
+++ go/buildall.py
@@ -0,0 +1,56 @@
+import os
+
+"""DEFUNCT
+darwin    arm
+darwin    arm64
+dragonfly    amd64
+freebsd    386
+freebsd    amd64
+freebsd    arm
+linux    386
+linux    arm64
+linux    ppc64le
+netbsd    386
+netbsd    amd64
+netbsd    arm
+openbsd    386
+openbsd    amd64
+openbsd    arm
+plan9    386
+plan9    amd64
+solaris    amd64
+windows    386
+"""
+
+arches = """darwin    386
+darwin    amd64
+linux    amd64
+linux    arm
+linux    ppc64
+windows    amd64"""
+
+arches = arches.split("\n")
+
+try:
+	os.system("rm -rf builds")
+except:
+	pass
+os.mkdir("builds")
+
+for arch in arches:
+	goos = arch.split()[0]
+	goarch = arch.split()[1]
+
+	exe = ""
+	if "windows" in goos:
+		exe = ".exe"
+	cmd1  = 'env GOOS=%(goos)s GOARCH=%(goarch)s%(exe)s go build -o builds/musicsaur-1.3-%(goos)s-%(goarch)s%(exe)s -v *.go' % {'goos':goos,'goarch':goarch,'exe':exe}
+	cmd2 = 'tar -cvzf builds/musicsaur-1.3-%(goos)s-%(goarch)s%(exe)s.tar.gz builds/musicsaur-1.3-%(goos)s-%(goarch)s%(exe)s' % {'goos':goos,'goarch':goarch,'exe':exe}
+	cmd3 = 'rm builds/musicsaur-1.3-%(goos)s-%(goarch)s%(exe)s' % {'goos':goos,'goarch':goarch,'exe':exe}
+	print(cmd1)
+	os.system(cmd1)
+	print(cmd2)
+	os.system(cmd2)
+	print(cmd3)
+	os.system(cmd3)
+
